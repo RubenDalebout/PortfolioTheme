df47c5ac810207f3d5ef98a8e1db2e8b20af4bcb
DeEchteZeeuw <rubendalebout@gmail.com>
2023-01-31 21:51:42 +0100

    removed some pages

--- inc/install/posts.php
+++ inc/install/posts.php
@@ -44,3 +44,18 @@ add_action( 'init', 'create_project_post_type' );
 
 // Add post thumbnail support for custom post type
 add_theme_support( 'post-thumbnails', array( 'project' ) );
+
+// Remove the Posts menu item and Comments menu item
+function remove_posts_comments_menu() {
+    remove_menu_page('edit.php');
+    remove_menu_page('edit-comments.php');
+}
+add_action( 'admin_menu', 'remove_posts_comments_menu' );
+
+// Redirect users trying to access the Posts and Comments pages
+function redirect_posts_comments_page() {
+    if (strpos($_SERVER['REQUEST_URI'], '/wp-admin/edit.php') !== false && !isset($_GET['post_type']) || strpos($_SERVER['REQUEST_URI'], '/wp-admin/edit-comments.php') !== false) {
+        wp_redirect(admin_url());
+    }
+}
+add_action( 'admin_init', 'redirect_posts_comments_page' );
