ecb961333ba6c03234cdd642ee8eb92cb631afc4
DeEchteZeeuw <rubendalebout@gmail.com>
2023-02-01 16:15:50 +0100

    Added the script file to include scripts and stylesheets

--- functions.php
+++ functions.php
@@ -7,3 +7,4 @@ include get_template_directory() . '/inc/install/taxonomies.php';
 include get_template_directory() . '/inc/install/metaboxs.php';
 include get_template_directory() . '/inc/install/pages.php';
 include get_template_directory() . '/inc/install/menus.php';
+include get_template_directory() . '/inc/install/scripts.php';
---
+++ inc/install/scripts.php
@@ -0,0 +1,22 @@
+<?php
+// Stop script if accessed directly
+if (!defined('ABSPATH')) exit;
+
+// Enqueue scripts on the pages
+function enqueue_theme_scripts() {
+    // Main js file
+    wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/js/main.min.js', array(), (wp_get_environment_type() === 'staging') ? time() : wp_get_theme()->get('Version'), false );
+    wp_localize_script( 'main', 'adminData', array( 
+        'site' => home_url(),
+    ));
+    
+    // Base stylesheet
+    wp_enqueue_style( 'base', get_template_directory_uri() . '/assets/css/base.min.css', array(), (wp_get_environment_type() === 'staging') ? time() : wp_get_theme()->get('Version') );
+}
+add_action('wp_enqueue_scripts', 'enqueue_theme_scripts');
+
+// Enqueue scripts on the admin
+function enqueue_theme_admin_scripts() {
+    
+}
+add_action('admin_enqueue_scripts', 'enqueue_theme_admin_scripts');
